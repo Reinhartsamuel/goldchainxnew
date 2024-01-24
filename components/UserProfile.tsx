4b926b3086cb91d9421551951ca7628d3a68ac2a
Reinhart Samuel <106574869+Reinhartsamuel@users.noreply.github.com>
2024-01-24 20:37:06 +0700

    add terima transfer qr

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -5,10 +5,12 @@ import { useWallet } from '@/app/context/walletContext'
 import { wallet } from '@/services/sequence'
 // import { wallet } from '@/services/sequence'
 import { trimAddress } from '@/services/utils'
-import { Box, HStack, Heading, Stack, useToast } from '@chakra-ui/react'
+import { Box, Button, HStack, Heading, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Stack, useDisclosure, useToast } from '@chakra-ui/react'
 import React, { ReactElement, useRef } from 'react'
 import { FaRegCopy } from 'react-icons/fa'
 import { LuWallet } from 'react-icons/lu'
+import { IoQrCode } from "react-icons/io5";
+import QRCode from "react-qr-code";
 
 const UserProfile = () => {
     const toast = useToast();
@@ -24,13 +26,14 @@ const UserProfile = () => {
             duration: 2000
         })
     };
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <>
             <Stack>
                 <HStack gap={2} alignSelf={'center'}>
                     <LuWallet />
-                    <Heading size='xs'color='orange'>My Wallet:</Heading>
+                    <Heading size='xs' color='orange'>My Wallet:</Heading>
                     <Heading size='sm'>
                         {trimAddress(walletAddress)}
                     </Heading>
@@ -45,7 +48,41 @@ const UserProfile = () => {
                         />
                     </Box>
                 </HStack>
+
+                <Button onClick={onOpen}>
+                    <HStack gap={2} alignSelf={'center'}>
+                        <IoQrCode />
+                        <Heading size='xs'>QR Terima Transfer</Heading>
+                    </HStack>
+                </Button>
             </Stack>
+
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                    <Heading size='sm'>
+                        {trimAddress(walletAddress)}
+                    </Heading>
+                        <QRCode
+                            size={256}
+                            level={'H'}
+                            style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                            value={walletAddress}
+                            viewBox={`0 0 256 256`}
+                        />
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme="orange" mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </>
     );
 };
